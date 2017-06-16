5c4b74139fa05e52e22614be1ee1cba28e8e0326
David Weber <dweb@Davids-MBP.fios-router.home>
2017-06-16 14:33:45 -0400

    created game logic

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: "Queen",
+  suit: "hearts",
+  cardImage: src = "images/queen-of-hearts.png"
+},
+{
+  rank: "Queen",
+  suit: "diamonds",
+  cardImage: src = "images/queen-of-diamonds.png"
+},
+{
+  rank: "King",
+  suit: "hearts",
+  cardImage: src = "images/king-of-hearts.png"
+},
+{
+  rank: "King",
+  suit: "diamonds",
+  cardImage: src = "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
   } else {
-      console.log("Sorry, try again.");
+      alert("Sorry, try again.");
   }
 };
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 	if (cardsInPlay.length === 2) {
     checkForMatch();
   }
